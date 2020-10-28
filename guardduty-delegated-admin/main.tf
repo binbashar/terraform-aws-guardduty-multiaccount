86652262547d8250a73f09da8b4e7abc92e7ded4
Diego OJ <diego.ojeda@binbash.com.ar>
2020-10-28 17:05:03 -0300

    Refactor module to remove mandatory provider

--- guardduty-delegated-admin/main.tf
+++ guardduty-delegated-admin/main.tf
@@ -1,10 +1,8 @@
 resource "aws_guardduty_detector" "this" {
-  provider                     = aws.main
   enable                       = var.guarduty_enabled
   finding_publishing_frequency = var.guarduty_finding_publishing_frequency
 }
 
 resource "aws_guardduty_organization_admin_account" "this" {
-  provider         = aws.main
   admin_account_id = var.guardduty_delegated_admin_account_id
 }
--- multiaccount-setup/main.tf
+++ multiaccount-setup/main.tf
@@ -4,7 +4,6 @@
 # when you set this account as a delegated admin.
 #
 resource "aws_guardduty_detector" "this" {
-  provider                      = aws.main
   enable                        = var.guarduty_enabled
   finding_publishing_frequency  = var.guarduty_finding_publishing_frequency
 }
@@ -12,7 +11,6 @@ resource "aws_guardduty_detector" "this" {
 # Set auto_enable to true if you want GuardDuty to be enabled in all of your
 # organization member accounts
 resource "aws_guardduty_organization_configuration" "this" {
-  provider    = aws.main
   auto_enable = var.guardduty_organization_members_auto_enable
   detector_id = aws_guardduty_detector.this.id
 }
@@ -22,8 +20,8 @@ resource "aws_guardduty_organization_configuration" "this" {
 # Note: a lifecycle is used because email and invite trigger changes every time.
 #
 resource "aws_guardduty_member" "members" {
-  provider    = aws.main
   for_each    = var.guardduty_member_accounts
+  
   account_id  = can(each.value.account_id) ? each.value.account_id : null
   detector_id = aws_guardduty_detector.this.id
   email       = can(each.value.email) ? each.value.email : null
