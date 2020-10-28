9d346ea1928de60158c7d2bc28d5096bca81dfa4
Diego OJ <diego.ojeda@binbash.com.ar>
2020-10-28 14:18:46 -0300

    Create separate modules for setting up GuardDuty using a multi-account setup and for configuring a delegated admin

--- guardduty-delegated-admin/main.tf
+++ guardduty-delegated-admin/main.tf
@@ -1,35 +1,10 @@
-#
-# Enable GuardDuty in this account.
-# Important: this needs to be imported as GuardDuty is automatically
-# when you set this account as a delegated admin.
-#
 resource "aws_guardduty_detector" "this" {
-  provider                      = aws.main
-  enable                        = var.guarduty_enabled
-  finding_publishing_frequency  = "SIX_HOURS"
+  provider                     = aws.main
+  enable                       = var.guarduty_enabled
+  finding_publishing_frequency = var.guarduty_finding_publishing_frequency
 }
 
-# Set auto_enable to true if you want GuardDuty to be enabled in all of your
-# organization member accounts
-resource "aws_guardduty_organization_configuration" "this" {
-  provider    = aws.main
-  auto_enable = var.guardduty_organization_members_auto_enable
-  detector_id = aws_guardduty_detector.this.id
-}
-
-#
-# Define explicitly all GuardDuty enabled accounts
-# Note: a lifecycle is used because email and invite trigger changes every time.
-#
-resource "aws_guardduty_member" "members" {
-  provider    = aws.main
-  # count       = length(var.guardduty_member_accounts)
-  for_each    = var.guardduty_member_accounts
-  account_id  = can(each.value.account_id) ? each.value.account_id : null
-  detector_id = aws_guardduty_detector.this.id
-  email       = can(each.value.email) ? each.value.email : null
-  invite      = can(each.value.invite) ? each.value.invite : false
-  lifecycle {
-    ignore_changes = [email, invite]
-  }
+resource "aws_guardduty_organization_admin_account" "this" {
+  provider         = aws.main
+  admin_account_id = var.guardduty_delegated_admin_account_id
 }
--- guardduty-delegated-admin/variables.tf
+++ guardduty-delegated-admin/variables.tf
@@ -4,14 +4,13 @@ variable "guarduty_enabled" {
     description = "Whether to enable GuardDuty or not."
 }
 
-variable "guardduty_organization_members_auto_enable" {
-    type        = bool
-    default     = true
-    description = "Whether to automatically enable GuardDuty on organization members or not."
+variable "guarduty_finding_publishing_frequency" {
+    type        = string
+    default     = "SIX_HOURS"
+    description = "The frequency of findings publishing."
 }
 
-variable "guardduty_member_accounts" {
-    type        = any
-    default     = []
-    description = "A collection of key-pairs that hold data about member accounts such as account_id, email and invite."
+variable "guardduty_delegated_admin_account_id" {
+    type        = number
+    description = "The account id of the account that will be GuardDuty's delegated admin."
 }
