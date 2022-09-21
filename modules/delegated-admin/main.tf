7d9fdc125d5c93480269b520a159745b0e73fce0
Mariano Di Modugno <mariano.dimodugno@binbash.com.ar>
2022-09-20 21:37:05 -0300

    Add Kubernetes and Malware protection config block con boolean enabled/disabled variable

--- modules/delegated-admin/main.tf
+++ modules/delegated-admin/main.tf
@@ -6,6 +6,18 @@ resource "aws_guardduty_detector" "this" {
     s3_logs {
       enable = var.guarduty_s3_protection_enabled
     }
+    kubernetes {
+      audit_logs {
+        enable = var.guarduty_kubernetes_protection_enabled
+      }
+    }
+    malware_protection {
+      scan_ec2_instance_with_findings {
+        ebs_volumes {
+          enable = var.guarduty_malware_protection_enabled
+        }
+      }
+    }
   }
 }
 
